21b3079c542aee1ad8c2189cacf885e4e35d8800
kalagam1 <kalagam1@udayton.edu>
2025-06-30 18:39:52 -0400

    Update email.js

--- email.js
+++ email.js
@@ -1,12 +1,12 @@
 var shown = false;
-function showhide(){
-if (shown){
-document.getElementById('email').innerHTML="show my email";
-shown = false;
+function showhide() {
+  if (shown) {
+    document.getElementById('email').innerHTML = "show my email";
+    shown = false;
+  } else {
+    var myemail = "<a href='mailto:kalagam1@udayton.edu'>kalagam1@udayton.edu</a>";
+    document.getElementById('email').innerHTML = myemail;
+    shown = true;
+  }
 }
-else{
-var myemail="<a href='mailto:kalagam1@udayton.edu"+"@"+"udayton.edu'>kalagam1"+"@"+"udayton.edu</a>";
-document.getElementById('email').innerHTML=myemail;
-shown=true;
-}
-}
+
